ecbdac5ca09fae6f6969d47b6febf5e71635b6b7
abin0001 <abinj2003@gamail.com>
2024-07-14 01:37:00 +0530

    completed

--- src/common/ThemeContext.jsx
+++ src/common/ThemeContext.jsx
@@ -15,7 +15,7 @@ export const ThemeProvider = ({ children }) => {
   }, [theme]);
 
   const toggleTheme = () => {
-    setTheme((prevTheme) => (prevTheme === 'dark' ? 'dark' : 'dark'));
+    setTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'));
   };
 
   return (
